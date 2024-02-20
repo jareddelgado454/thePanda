4a00f1d87ec502aca1be8701716e79b79936b041
DelioCoder <davideveriwhere@gmail.com>
2024-02-20 17:42:19 -0500

    design fixed

--- client/src/app/admin-dashboard/layout.jsx
+++ client/src/app/admin-dashboard/layout.jsx
@@ -7,8 +7,8 @@ export default function AdminDashboardLayout ({children}) {
     return(
         <div className="flex w-full h-screen max-h-screen">
             <Sidebar />
-            <div className="home_content transition-all ease-out duration-500 p-4">
-                <div className="text-white bg-slate-300 h-full rounded-lg dark:bg-green-panda">
+            <div className="home_content transition-all ease-out duration-500 px-2 py-4">
+                <div className="text-white bg-slate-300 h-full rounded-lg dark:bg-green-panda overflow-hidden">
                 {
                     children
                 }
