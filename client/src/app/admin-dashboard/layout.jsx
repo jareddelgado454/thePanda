4aa630b154210b5f5f06eef831f146f77e5431f3
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2024-02-20 17:12:00 -0500

    Cards and home

--- client/src/app/admin-dashboard/layout.jsx
+++ client/src/app/admin-dashboard/layout.jsx
@@ -8,7 +8,7 @@ export default function AdminDashboardLayout ({children}) {
         <div className="flex w-full h-screen max-h-screen">
             <Sidebar />
             <div className="home_content transition-all ease-out duration-500 p-4">
-                <div className="text-white bg-slate-300 h-full rounded-lg dark:bg-green-panda">
+                <div className="text-white bg-slate-300 h-full rounded-lg dark:bg-green-panda p-8">
                 {
                     children
                 }
---
+++ client/src/components/CardHome.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { RiTeamFill, RiTaxiFill, RiGroupFill } from "react-icons/ri";
+
+const CardHome = ({type, numberActive, lastRegistrations}) => {
+  return (
+    <div className='dark:bg-zinc-800 w-[25%] h-[220px] rounded-xl shadow-2xl px-4 py-5 flex flex-col justify-between'>
+        <div className='w-full flex justify-between items-center'>
+            {
+                type == "Customers" ? (
+                    <RiGroupFill className='text-[50px]'/>
+                ) : type == "Technicians" ? (
+                    <RiTaxiFill className='text-[50px]'/>
+                ) : (
+                    <RiTeamFill className='text-[50px]'/>
+                )
+            }
+            
+            <h3 className='text-[50px] font-bold'>{numberActive}</h3>
+        </div>
+        <div className='w-full flex justify-between items-center'>
+            <h4 className='font-bold text-[20px]'>{type}</h4>
+            {
+                type == "Customers" ? (
+                    <h4>Last Customers: +{lastRegistrations}</h4>
+                ) : type == "Technicians" ? (
+                    <h4>Last Technicians: +{lastRegistrations}</h4>
+                ) : (
+                    <h4>Last Users: +{lastRegistrations}</h4>
+                )
+            }
+        </div>
+    </div>
+  )
+}
+
+export default CardHome
