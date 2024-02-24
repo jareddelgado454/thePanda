a74c91d9fde2a3250a5d66d1287e6dcf13a181a7
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2024-02-24 10:15:10 -0500

    modularization

---
+++ client/src/graphql/users/mutation.js
@@ -0,0 +1,12 @@
+import { gql} from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation($email: String!, $password: String!){
+    login(email: $email, password: $password) {
+        id
+        email
+        createdAt
+        token
+    }
+  }
+`;
